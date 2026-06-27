bf616fbe757ce623bc1a6544d3a366806b18688e
edawg <muhammadridho5654@gmail.com>
2026-06-27 11:19:43 +0700

    fix: migrtaion

--- prisma/migrations/20260627120000_spk_termin_scheme/migration.sql
+++ prisma/migrations/20260627120000_spk_termin_scheme/migration.sql
@@ -1,20 +1,41 @@
--- CreateEnum
-CREATE TYPE "SpkTerminScheme" AS ENUM ('rumah_default', 'infra_20_6', 'infra_30_4');
+-- Skema termin per SPK + enum termin infra 30/30/30/10 (MySQL)
 
--- AlterEnum SpkPembayaranJenis
-ALTER TYPE "SpkPembayaranJenis" ADD VALUE 'termin_infra_30_1';
-ALTER TYPE "SpkPembayaranJenis" ADD VALUE 'termin_infra_30_2';
-ALTER TYPE "SpkPembayaranJenis" ADD VALUE 'termin_infra_30_3';
-ALTER TYPE "SpkPembayaranJenis" ADD VALUE 'termin_infra_10';
+ALTER TABLE `spk`
+  ADD COLUMN `termin_scheme` ENUM('rumah_default', 'infra_20_6', 'infra_30_4') NOT NULL DEFAULT 'rumah_default';
 
--- AlterEnum SpkKasbonTargetTermin
-ALTER TYPE "SpkKasbonTargetTermin" ADD VALUE 'termin_infra_30_1';
-ALTER TYPE "SpkKasbonTargetTermin" ADD VALUE 'termin_infra_30_2';
-ALTER TYPE "SpkKasbonTargetTermin" ADD VALUE 'termin_infra_30_3';
-ALTER TYPE "SpkKasbonTargetTermin" ADD VALUE 'termin_infra_10';
+UPDATE `spk`
+SET `termin_scheme` = 'infra_20_6'
+WHERE `jenis` = 'infrastruktur';
 
--- AlterTable
-ALTER TABLE "spk" ADD COLUMN "termin_scheme" "SpkTerminScheme" NOT NULL DEFAULT 'rumah_default';
+ALTER TABLE `spk_pembayaran`
+  MODIFY `jenis` ENUM(
+    'termin_55',
+    'termin_100',
+    'termin_infra_20_1',
+    'termin_infra_20_2',
+    'termin_infra_20_3',
+    'termin_infra_20_4',
+    'termin_infra_15',
+    'termin_infra_30_1',
+    'termin_infra_30_2',
+    'termin_infra_30_3',
+    'termin_infra_10',
+    'retensi',
+    'kasbon',
+    'upah'
+  ) NOT NULL;
 
--- Backfill: SPK infrastruktur existing → skema lama
-UPDATE "spk" SET "termin_scheme" = 'infra_20_6' WHERE "jenis" = 'infrastruktur';
+ALTER TABLE `spk_pembayaran`
+  MODIFY `mengurangi_termin` ENUM(
+    'termin_55',
+    'termin_100',
+    'termin_infra_20_1',
+    'termin_infra_20_2',
+    'termin_infra_20_3',
+    'termin_infra_20_4',
+    'termin_infra_15',
+    'termin_infra_30_1',
+    'termin_infra_30_2',
+    'termin_infra_30_3',
+    'termin_infra_10'
+  ) NULL;
